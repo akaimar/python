0e4d68a555441825fe0f8bd97f525dc7d3e8a020
akaimar <39114801+akaimar@users.noreply.github.com>
2020-10-08 17:00:25 +0300

    Update sajaakrimets.py

--- praktika/sajaakrimets.py
+++ praktika/sajaakrimets.py
@@ -1,3 +1,5 @@
+# Fail: saja_aakri_mets_andmed.txt
+
 #1 aaker = 0,4047 ha
 
 #Omanik tahab teada, mitu tihumeetrit metsa aastas teatud suurusest
@@ -29,3 +31,36 @@ väljastab lõpuks ekraanile, mitme metsatüki juurdekasv arvutati."""
 
 # Arvutamiseks võib kasutada valemit: (metsatüki juurdekasv) =
 #(metsatüki pindala aakrites) * 0.4047 * (aastane juurdekasv)
+
+''' ADNMED FAILIS '''
+
+failinimi = input('Sisesta faili nimi (andmed.txt): ') #küsime faili nime
+
+pindalad = [] #teeme tühja listi
+
+fail = open(failinimi, encoding = 'UTF-8') #avame faili
+
+for rida in fail: #teeme tsükli failidest ridade lugemiseks
+    pindalad.append(float(rida)) #lisame pindalade järjendisse
+    
+fail.close()
+
+''' FUNKTSIOON ARVUTAMISEKS '''
+
+kasv = float(input("Sisestage aastene juurde kasv aastas hetkari kohta tihumeetrit: "))
+piir = float(input("Sisesta piiri, mitmest aakrist suuremad metsatükid arvesse võtta: "))
+
+def juurdekasv(pindala, juurdekasv): 
+    #tagastab metsatüki aastase juurdekasvu ümardatuna sajandikeni
+    return round(pindala * 0.4047 * juurdekasv,2)
+
+kokku = 0
+
+for el in pindalad:
+    if el < piir:
+        print(el, 'seda metsatükki ei võeta arvesse')
+    else:
+        print("Metsatüki aastane juurdekasv on", juurdekasv(el,kasv))
+        kokku += 1
+        
+print("Arvutati ", kokku, " metsatüki juurdekasv")
